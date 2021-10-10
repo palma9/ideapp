7809db7ddeafdf6ef2c15c0e3fee19a631253240
Jesus Palma <jpalmalop@gmail.com>
2021-10-10 16:53:21 +0200

    feature: ideas timeline

--- app/ideas/schema.py
+++ app/ideas/schema.py
@@ -11,6 +11,7 @@ from ideas.models import Idea
 class Query(graphene.ObjectType):
     my_ideas = graphene.List(IdeaType)
     user_ideas = graphene.List(IdeaType, user_id=graphene.Int(required=True))
+    ideas_timeline = graphene.List(IdeaType)
 
     @login_required
     def resolve_my_ideas(self, info):
@@ -22,13 +23,23 @@ class Query(graphene.ObjectType):
         """ Return a list of ideas from an user """
         my_user = info.context.user
         target_user = CustomUser.objects.get(id=user_id)
+        private_ideas = Idea.VisibilityChoices.private.value
         if my_user.id and user_id == my_user.id:  # Show all my ideas
             filter = Q(user_id = my_user.id)
         elif my_user.id and target_user in my_user.my_following:  # I'm following him, so show public and protected ones
-            filter = Q(user_id = user_id) & ~Q(visibility=Idea.VisibilityChoices.private.value)
+            filter = Q(user_id = user_id) & ~Q(visibility=private_ideas)
         else:  # People I'm not following. Only can see public ideas
             filter = Q(user_id = user_id) & Q(visibility=Idea.VisibilityChoices.public.value)
         return Idea.objects.filter(filter).order_by('-date_created')
+
+    @login_required
+    def resolve_ideas_timeline(self, info):
+        """ Return a timeline of ideas from users I'm following and mine """
+        my_user = info.context.user
+        my_following = my_user.my_following
+        private_ideas = Idea.VisibilityChoices.private.value
+        return Idea.objects.filter(Q(user=my_user) | Q(user__in=my_following) & ~Q(visibility=private_ideas)).order_by('date_created')
+
 
 class Mutation(graphene.ObjectType):
     create_idea = createIdea.Field()
