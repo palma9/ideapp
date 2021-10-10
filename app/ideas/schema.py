99ce0a7cdafb43a8133cda773cf4882a0c0ea15f
Jesus Palma <jpalmalop@gmail.com>
2021-10-10 16:38:08 +0200

    feature: user_ideas

--- app/ideas/schema.py
+++ app/ideas/schema.py
@@ -1,5 +1,8 @@
+from django.db.models.query_utils import Q
 import graphene
 from graphql_jwt.decorators import login_required
+from users.models import CustomUser
+from users.types import UserType
 
 from ideas.mutations import createIdea, updateIdeaVisibility, deleteIdea
 from ideas.types import IdeaType
@@ -7,12 +10,25 @@ from ideas.models import Idea
 
 class Query(graphene.ObjectType):
     my_ideas = graphene.List(IdeaType)
+    user_ideas = graphene.List(IdeaType, user_id=graphene.Int(required=True))
 
     @login_required
     def resolve_my_ideas(self, info):
         """ Return a list of my ideas """
         my_user = info.context.user
         return Idea.objects.filter(user__id=my_user.id).order_by('-date_created')
+
+    def resolve_user_ideas(self, info, user_id):
+        """ Return a list of ideas from an user """
+        my_user = info.context.user
+        target_user = CustomUser.objects.get(id=user_id)
+        if my_user.id and user_id == my_user.id:  # Show all my ideas
+            filter = Q(user_id = my_user.id)
+        elif my_user.id and target_user in my_user.my_following:  # I'm following him, so show public and protected ones
+            filter = Q(user_id = user_id) & ~Q(visibility=Idea.VisibilityChoices.private.value)
+        else:  # People I'm not following. Only can see public ideas
+            filter = Q(user_id = user_id) & Q(visibility=Idea.VisibilityChoices.public.value)
+        return Idea.objects.filter(filter).order_by('-date_created')
 
 class Mutation(graphene.ObjectType):
     create_idea = createIdea.Field()
